7f88c607591c7b4e9ad8010acc5d55a9cef5b09f
edoatley <edoatley@users.noreply.github.com>
2025-03-15 15:30:08 +0000

    start adding labs and structure

---
+++ scripts/teradown.sh
@@ -0,0 +1,10 @@
+#!/bin bash
+STACK_FOLDER=${1:-network}
+STACK_NAME=${2:-simple-vpc}
+
+# call the aws-cfn.yaml workflow passing the parameters in
+gh workflow run aws-cfn.yaml \
+  -p cloudformation_template_folder=$STACK_FOLDER \
+  -p cloudformation_stack_name=$STACK_NAME \
+  -p cloudformation_action=delete
+ 
